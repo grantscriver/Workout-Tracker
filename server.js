06c2078d647519271affb166316c9dfe58111c9b
Grant Scriver <grantscriver@gmail.com>
2020-11-06 15:42:47 -0600

    deploying

--- server.js
+++ server.js
@@ -11,11 +11,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Workout-Tracker',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // routes
 app.use(require("./routes/api.js"));
