0ca55a92530586b6c687d399f410a8f3e6dbfc65
Grant Scriver <grantscriver@gmail.com>
2020-11-06 14:57:07 -0600

    Got api routes to display the dashboard

--- routes/api.js
+++ routes/api.js
@@ -37,4 +37,14 @@ router.put("/api/workouts/:id", (req, res) => {
         });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router
